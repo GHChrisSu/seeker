fd3677b6f28c53a85009395b7c0d19f65868b88e
suhao <chrissusuhao@gmail.com>
2024-06-02 21:34:51 +0800

    add my yun pan

--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --net ngroknet -p 8080:8080 --mount type=bind,source="$(pwd)",target=/root/seeker --name seeker thewhiteh4t/seeker
+docker run --rm -it -p 8080:8080 --mount type=bind,source="$(pwd)",target=/root/seeker --name seeker thewhiteh4t/seeker
